5086c7f109b2487274cb4183456070d5f3848544
minette.alexandre@gmail.com <perso@pelos-K53SC.home>
2013-12-08 15:54:36 +0100

    changing permission on apache conf folder

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,13 @@ file {'/deploy':
       owner => user-deploy,
       group => user-deploy,
 } ->
+file { '/deploy/.bashrc':
+      ensure => 'present',
+      owner => user-deploy,
+      mode    => 0640,
+      group => user-deploy,
+      content => "alias ll='ls-l'; alias ne='emacs -nw'",     
+} ->
 file { '/deploy/.forward':
       ensure => 'present',
       owner => user-deploy,
@@ -47,6 +54,15 @@ package { "acl":
 } ->
 exec { "setfacl -m g:user-deploy:rwx /var/www/":
   command => "setfacl -m g:user-deploy:rwx /var/www/",
+  path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
+}
+exec { "setfacl -m g:user-deploy:rwx /etc/apache2/site-enabled/":
+  command => "setfacl -m g:user-deploy:rwx /etc/apache2/site-enabled/",
+  path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
+} 
+
+exec { "setfacl -m g:user-deploy:rwx /etc/apache2/site-available/":
+  command => "setfacl -m g:user-deploy:rwx /etc/apache2/site-available/",
   path => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
 } 
 
